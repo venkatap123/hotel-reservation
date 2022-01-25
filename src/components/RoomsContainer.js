54b052e86f96c029d549752a7deba8a9489476d3
venkatap123 <98193489+venkatap123@users.noreply.github.com>
2022-01-24 18:28:02 -0800

    Rooms list added

---
+++ src/components/RoomsContainer.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { RoomConsumer } from "../context";
+import Loading from "./Loading";
+import RoomsFilter from "./RoomsFilter";
+import RoomsList from "./RoomsList";
+export default function RoomContainer() {
+  return (
+    <RoomConsumer>
+      {value => {
+        const { loading, setRoom, sortedRooms,rooms } = value;
+        if (loading) {
+          return <Loading />;
+        }
+        return (
+          <>
+            <RoomsFilter rooms={rooms} />
+            <RoomsList rooms={sortedRooms} setRoom={setRoom} />
+          </>
+        );
+      }}
+    </RoomConsumer>
+  );
+}
--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import Hero from '../components/Hero';
 import Banner from '../components/Banner';
+import RoomContainer from '../components/RoomsContainer'
 
 import { Link } from 'react-router-dom';
 
 export default function Rooms() {
-  return  <Hero hero="roomsHero">
-  <Banner title="Our Rooms" 
+  return  (
+  <>
+  <Hero hero="roomsHero">
+    <Banner title="Our Rooms" 
     subtitle="Rooms starting from $299">
       <Link to="/" className='btn-primary'>Home</Link>
     </Banner>
-</Hero>;
+  </Hero>
+  <RoomContainer />
+  
+  </>
+  );
 }
 
